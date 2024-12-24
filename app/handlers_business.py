7a37fcbb66e32a49369af4c0aad725edd10a63e1
Igor <89027688149@ya.ru>
2024-12-24 20:59:16 +0800

    edit handlers_business.py

--- app/handlers_business.py
+++ app/handlers_business.py
@@ -1,9 +1,16 @@
+# aiogram
+import pprint
+
 from aiogram import Router
 from aiogram.types import Message
-from db.sqlite import db_start, check_answer_on_question, add_message, db_stop
 
+# sqlite
+from db.sqlite import db_start, check_answer_on_question, add_message, get_question_for_chats, db_stop
+
+# my import
 from message.parser_template_messages import *
 from config.parser_config_business import get_active_business
+from config.parser_config_admin import get_owner_user_id
 
 router = Router()
 
@@ -20,37 +27,19 @@ async def handler(message: Message):
         check_answer_2 = await check_answer_on_question(user_id=message.from_user.id, num_question=2)
         check_answer_3 = await check_answer_on_question(user_id=message.from_user.id, num_question=3)
 
-        if not check_answer_0:
+        list_check_answer = [check_answer_0, check_answer_1, check_answer_2, check_answer_3]
 
-            await add_message(user_id=message.from_user.id, num_question=0, answer=message.text)
-            emoji_got_it = ReactionTypeEmoji(emoji='👍')
-            await message.react(reaction=[emoji_got_it])
-            await message.answer(text=get_welcome_messages())
-            await message.answer(text=get_question_1())
-
-        elif not check_answer_1:
-
-            emoji_got_it = ReactionTypeEmoji(emoji='👍')
-            await message.react(reaction=[emoji_got_it])
-            await add_message(user_id=message.from_user.id, num_question=1, answer=message.text)
-            await message.answer(text=get_question_2())
-
-        elif not check_answer_2:
-
-            emoji_got_it = ReactionTypeEmoji(emoji='👍')
-            await message.react(reaction=[emoji_got_it])
-            await add_message(user_id=message.from_user.id, num_question=2, answer=message.text)
-            await message.answer(text=get_question_3())
-
-        elif not check_answer_3:
-
-            emoji_got_it = ReactionTypeEmoji(emoji='👍')
-            await message.react(reaction=[emoji_got_it])
-            await add_message(user_id=message.from_user.id, num_question=3, answer=message.text)
-            await message.answer(text=all_answers_get())
-
-        elif check_answer_1 and check_answer_2 and check_answer_3:
-
-            await message.answer(text=all_answers_get())
+        if not all(list_check_answer):
+            for index, answer in enumerate(list_check_answer):
+                if not answer:
+                    await add_message(user_id=message.from_user.id, num_question=index, answer=message.text)
+                    if index == 0:
+                        await message.answer(text=get_welcome_messages())
+                        await message.answer(text=get_question(index + 1))
+                    elif index == len(list_check_answer) - 1:
+                        await message.answer(text=all_answers_get())
+                    else:
+                        await message.answer(text=get_question(index + 1))
+                    break
 
         await db_stop()
